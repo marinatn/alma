31e5c6171479a0e75f2398a1b587811bff060cf4
Marina Trizna <marina.tna@yandex.ru>
2022-06-12 08:24:15 +0300

    Update brands-slider-duble.php

--- includes/brands-slider-duble.php
+++ includes/brands-slider-duble.php
@@ -4,7 +4,7 @@
 			<div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
 
 				<div class="item">
-					<a href="#" class="image">
+					<a href="http://alma/category.php?cid=23" class="image">
 						<img data-echo="brandsimage/mdp.jpg" src="assets/images/blank.gif" alt="">
 					</a>	
 				</div><!--/.item-->
@@ -16,31 +16,31 @@
 				</div><!--/.item-->
 
 				<div class="item">
-					<a href="#" class="image">
+					<a href="http://alma/category.php?cid=27" class="image">
 						<img data-echo="brandsimage/pantika.jpg" src="assets/images/blank.gif" alt="">
 					</a>	
 				</div><!--/.item-->
 
 				<div class="item">
-					<a href="#" class="image">
+					<a href="http://alma/category.php?cid=30" class="image">
 						<img data-echo="brandsimage/poliada.jpg" src="assets/images/blank.gif" alt="">
 					</a>	
 				</div><!--/.item-->
 
 				<div class="item">
-					<a href="#" class="image">
+					<a href="http://alma/category.php?cid=31" class="image">
 						<img data-echo="brandsimage/rose.jpg" src="assets/images/blank.gif" alt="">
 					</a>	
 				</div><!--/.item-->
 
 				<div class="item">
-					<a href="#" class="image">
+					<a href="http://alma/category.php?cid=32" class="image">
 						<img data-echo="brandsimage/sakskie.jpg" src="assets/images/blank.gif" alt="">
 					</a>	
 				</div><!--/.item-->
 
 				<div class="item">
-					<a href="#" class="image">
+					<a href="http://alma/category.php?cid=13" class="image">
 						<img data-echo="brandsimage/doctor_oil.jpg" src="assets/images/blank.gif" alt="">
 					</a>	
 				</div><!--/.item-->

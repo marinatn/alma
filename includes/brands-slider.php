04ff1b8503bfb86ecf3211984294890823f0183e
Marina Trizna <marina.tna@yandex.ru>
2022-06-12 08:22:16 +0300

    Update brands-slider.php

--- includes/brands-slider.php
+++ includes/brands-slider.php
@@ -4,59 +4,108 @@
 			<div id="brand-slider" class="owl-carousel brand-slider custom-carousel owl-theme">
 
 				<div class="item">
-					<a href="#" class="image">
+					<a href="http://alma/category.php?cid=11" class="image">
 						<img data-echo="brandsimage/bioteka.jpg" src="assets/images/blank.gif" alt="">
 					</a>	
 				</div><!--/.item-->
 
 				<div class="item">
-					<a href="#" class="image">
+					<a href="http://alma/category.php?cid=12" class="image">
 						<img data-echo="brandsimage/botavikos.jpg" src="assets/images/blank.gif" alt="">
 					</a>	
 				</div><!--/.item-->
-				
+
 				<div class="item">
-					<a href="#" class="image">
+					<a href="http://alma/category.php?cid=14" class="image">
 						<img data-echo="brandsimage/ecocraft.jpg" src="assets/images/blank.gif" alt="">
 					</a>	
 				</div><!--/.item-->
 
 				<div class="item">
-					<a href="#" class="image">
+					<a href="http://alma/category.php?cid=15" class="image">
 						<img data-echo="brandsimage/elisia.jpg" src="assets/images/blank.gif" alt="">
 					</a>	
 				</div><!--/.item-->
 
 				<div class="item">
-					<a href="#" class="image">
+					<a href="http://alma/category.php?cid=16" class="image">
 						<img data-echo="brandsimage/freshbubble.jpg" src="assets/images/blank.gif" alt="">
 					</a>	
 				</div><!--/.item-->
 
 				<div class="item">
-					<a href="#" class="image">
+					<a href="http://alma/category.php?cid=17" class="image">
 						<img data-echo="brandsimage/innature.jpg" src="assets/images/blank.gif" alt="">
 					</a>	
 				</div><!--/.item-->
 
 				<div class="item">
-					<a href="#" class="image">
+					<a href="http://alma/category.php?cid=18" class="image">
 						<img data-echo="brandsimage/js.jpg" src="assets/images/blank.gif" alt="">
 					</a>	
 				</div><!--/.item-->
 
 				<div class="item">
-					<a href="#" class="image">
+					<a href="http://alma/category.php?cid=21" class="image">
 						<img data-echo="brandsimage/lab.jpg" src="assets/images/blank.gif" alt="">
 					</a>	
 				</div><!--/.item-->
 
 				<div class="item">
-					<a href="#" class="image">
+					<a href="http://alma/category.php?cid=20" class="image">
 						<img data-echo="brandsimage/km.jpg" src="assets/images/blank.gif" alt="">
 					</a>	
 				</div><!--/.item-->
 				
+				<div class="item">
+					<a href="http://alma/category.php?cid=19" class="image">
+						<img data-echo="brandsimage/kleona.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="http://alma/category.php?cid=22" class="image">
+						<img data-echo="brandsimage/levrana.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="http://alma/category.php?cid=24" class="image">
+						<img data-echo="brandsimage/miko.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="http://alma/category.php?cid=25" class="image">
+						<img data-echo="brandsimage/mom.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="http://alma/category.php?cid=33" class="image">
+						<img data-echo="brandsimage/shokonat.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="http://alma/category.php?cid=34" class="image">
+						<img data-echo="brandsimage/smorodina.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="http://alma/category.php?cid=35" class="image">
+						<img data-echo="brandsimage/tintberry.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+				<div class="item">
+					<a href="http://alma/category.php?cid=36" class="image">
+						<img data-echo="brandsimage/true_alchemy.jpg" src="assets/images/blank.gif" alt="">
+					</a>	
+				</div><!--/.item-->
+
+
 		    </div><!-- /.owl-carousel #logo-slider -->
 		</div><!-- /.logo-slider-inner -->
 	

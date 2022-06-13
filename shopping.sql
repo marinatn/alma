1b9c6c01ad5380316f8d5da03e6d5f5e39eab106
Marina Trizna <marina.tna@yandex.ru>
2022-06-13 13:09:07 +0300

    Update shopping.sql

--- shopping.sql
+++ shopping.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Июн 13 2022 г., 13:08
+-- Время создания: Июн 09 2022 г., 14:44
 -- Версия сервера: 5.7.38-log
 -- Версия PHP: 7.1.33
 
@@ -40,7 +40,7 @@ CREATE TABLE `admin` (
 --
 
 INSERT INTO `admin` (`id`, `username`, `password`, `creationDate`, `updationDate`) VALUES
-(1, 'admin', 'test123', '2017-01-24 16:21:18', '21-06-2018 08:27:55 PM');
+(1, 'admin', 'f925916e2754e5e03f75dd58a5733251', '2017-01-24 16:21:18', '21-06-2018 08:27:55 PM');
 
 -- --------------------------------------------------------
 
@@ -61,32 +61,8 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`id`, `categoryName`, `categoryDescription`, `creationDate`, `updationDate`) VALUES
-(11, 'Bioteka ', 'Натуральные и органические продукты питания, напитки и пищевые добавки.', '2022-06-12 04:59:23', '12-06-2022 11:16:49 AM'),
-(12, 'Botavikos', 'Косметика Botavikos – растительный уход, который помогает восстановить естественные защитные силы, гармонизировать физиологические процессы, помочь коже и волосам стать здоровыми.', '2022-06-12 05:00:02', NULL),
-(13, 'Doctor-Oil', 'Уникальный в своем роде производитель Doctor Oil . Производство расположено на Крымском побережье Черного моря, в городе — курорте Ялта. В ассортименте производителя только натуральные и органические нативные средства.', '2022-06-12 05:02:11', '12-06-2022 08:02:30 AM'),
-(14, 'Ecocraft', 'Создана по уникальным рецептам мастеров из Грасса – мировой столицы французской парфюмерии.\r\nКаждый аромат из серии ECOCRAFT рассказывает отдельную историю, открывает новый мир.', '2022-06-12 05:03:16', NULL),
-(15, 'Elisia', 'Отечественный бренд растительной косметики для ухода за кожей с гармоничным сочетанием активных и натуральных компонентов, эффективность которых подтверждена исследованиями и проверена временем', '2022-06-12 05:04:31', NULL),
-(16, 'Freshbubble', 'Средства из растительных ингредиентов для уборки дома. Эко-средства Freshbubble безопасны для здоровья и окружающей среды', '2022-06-12 05:04:52', NULL),
-(17, 'INNATURE', 'Продукция марки Innature производится в России и использует местные и экзотические природные компоненты для создания натуральных средств по уходу за кожей, лицом и волосами', '2022-06-12 05:05:45', NULL),
-(18, 'Jurassic Spa', '', '2022-06-12 05:07:21', NULL),
-(19, 'Kleona', '', '2022-06-12 05:07:34', NULL),
-(20, 'Kristall Minerals', '', '2022-06-12 05:07:54', NULL),
-(21, 'Laboratorium', '', '2022-06-12 05:08:03', NULL),
-(22, 'Levrana', '', '2022-06-12 05:08:09', NULL),
-(23, 'Мануфактура \"Дом природы\"', '', '2022-06-12 05:08:24', NULL),
-(24, 'MiKo', '', '2022-06-12 05:08:55', NULL),
-(25, 'Мастерская Олеси Мустаевой', '', '2022-06-12 05:09:05', NULL),
-(26, 'Nano Organic', '', '2022-06-12 05:09:14', NULL),
-(27, 'Натуральная Крымская коллекция', '', '2022-06-12 05:09:32', NULL),
-(28, 'Organic Zone!', '', '2022-06-12 05:09:43', NULL),
-(29, 'ПАNTIKA', '', '2022-06-12 05:10:01', NULL),
-(30, 'Полиада-Крым', '', '2022-06-12 05:10:14', NULL),
-(31, 'Крымская роза', '', '2022-06-12 05:10:22', NULL),
-(32, 'Сакские грязи', '', '2022-06-12 05:10:35', NULL),
-(33, 'Shokonat', '', '2022-06-12 05:10:45', NULL),
-(34, 'SmoRodina', '', '2022-06-12 05:10:59', NULL),
-(35, 'TintBerry', '', '2022-06-12 05:11:11', NULL),
-(36, 'True Alchemy', '', '2022-06-12 05:11:24', NULL);
+(7, 'Шампуни', '', '2020-11-26 15:36:42', '07-06-2022 02:08:29 PM'),
+(10, 'Для лица', NULL, '2022-06-08 05:34:10', NULL);
 
 -- --------------------------------------------------------
 
@@ -112,14 +88,7 @@ INSERT INTO `orders` (`id`, `userId`, `productId`, `quantity`, `orderDate`, `pay
 (7, 1, '21', 1, '2020-11-26 16:05:13', 'Internet Banking', NULL),
 (8, 5, '21', 1, '2022-06-09 11:00:53', 'Debit / Credit card', NULL),
 (9, 1, '21', 1, '2022-06-09 11:02:37', NULL, NULL),
-(10, 1, '21', 1, '2022-06-09 11:39:07', NULL, NULL),
-(11, 1, '21', 1, '2022-06-09 12:35:23', NULL, NULL),
-(12, 1, '21', 1, '2022-06-09 12:47:24', NULL, NULL),
-(13, 5, '21', 5, '2022-06-11 16:26:10', NULL, 'in Process'),
-(14, 5, '21', 7, '2022-06-12 03:52:09', NULL, NULL),
-(15, 5, '22', 1, '2022-06-12 14:17:36', NULL, NULL),
-(16, 5, '26', 1, '2022-06-12 14:17:36', NULL, NULL),
-(17, 5, '29', 1, '2022-06-12 14:17:36', NULL, NULL);
+(10, 1, '21', 1, '2022-06-09 11:39:07', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -134,13 +103,6 @@ CREATE TABLE `ordertrackhistory` (
   `remark` longtext COLLATE utf8mb4_unicode_ci,
   `postingDate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Дамп данных таблицы `ordertrackhistory`
---
-
-INSERT INTO `ordertrackhistory` (`id`, `orderId`, `status`, `remark`, `postingDate`) VALUES
-(1, 13, 'in Process', 'Заказ доставлен до пункта выдачи', '2022-06-11 16:27:40');
 
 -- --------------------------------------------------------
 
@@ -189,13 +151,7 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `category`, `subCategory`, `productName`, `productCompany`, `productPrice`, `productPriceBeforeDiscount`, `productDescription`, `productImage1`, `productImage2`, `productImage3`, `shippingCharge`, `productAvailability`, `postingDate`, `updationDate`) VALUES
-(21, 7, 13, 'шамп', 'Uniqlo', 20, 50, '<span style=\"color: rgb(0, 0, 0); font-family: Helvetica, sans-serif; font-size: 16px;\">Uniqlo and the artist KAWS are teaming up again for a unique capsule collection. This exceptional collaboration revealed on social networks includes several t-shirts and totebags. After several other partnerships, this one highlights the characters of the capsule collection.&nbsp;</span><strong style=\"box-sizing: border-box; color: rgb(0, 0, 0); font-family: Helvetica, sans-serif; font-size: 16px;\">COMPANION</strong><span style=\"color: rgb(0, 0, 0); font-family: Helvetica, sans-serif; font-size: 16px;\">and&nbsp;</span><strong style=\"box-sizing: border-box; color: rgb(0, 0, 0); font-family: Helvetica, sans-serif; font-size: 16px;\">BFF</strong><span style=\"color: rgb(0, 0, 0); font-family: Helvetica, sans-serif; font-size: 16px;\">&nbsp;invented by KAWS.</span><br>', '1.jpg', '1.jpg', '1.jpg', 2, 'In Stock', '2020-11-26 15:44:51', NULL),
-(22, 15, 14, 'Анти-акне, пакет 25 гр', 'Elisia', 214, 214, '<br>', '2.jpg', '2.1.jpg', '', 20, 'In Stock', '2022-06-12 09:36:48', NULL),
-(25, 15, 14, 'Против купероза, пакет 25 гр', 'Elisia', 214, 214, '<br>', '4.jpg', '4.1.jpg', '', 20, 'In Stock', '2022-06-12 09:41:54', NULL),
-(26, 15, 15, 'Увлажнение 24 часа, ампулы 2 мл*10 шт', 'Elisia', 637, 637, '<br>', '5.jpg', '5.1.jpg', '', 20, 'In Stock', '2022-06-12 09:43:19', NULL),
-(27, 15, 15, 'Восточный эликсир, ампулы 2 мл * 10 шт', 'Elisia', 637, 637, '<br>', '6.jpg', '6.1.jpg', '6.2.jpg', 20, 'Out of Stock', '2022-06-12 09:44:38', NULL),
-(28, 15, 14, 'Отбеливающая маска для лица, пакет 25 гр', 'Elisia', 214, 214, '<br>', '3.jpg', '3.1.jpg', '', 20, 'In Stock', '2022-06-12 09:47:04', NULL),
-(29, 16, 21, 'Мыло жидкое \"Сладкий апельсин\", 300 мл', 'Freshbuble', 176, 220, '<br>', '7.jpg', '7.jpg', '', 20, 'Out of Stock', '2022-06-12 09:49:21', NULL);
+(21, 7, 13, 'шамп', 'Uniqlo', 20, 50, '<span style=\"color: rgb(0, 0, 0); font-family: Helvetica, sans-serif; font-size: 16px;\">Uniqlo and the artist KAWS are teaming up again for a unique capsule collection. This exceptional collaboration revealed on social networks includes several t-shirts and totebags. After several other partnerships, this one highlights the characters of the capsule collection.&nbsp;</span><strong style=\"box-sizing: border-box; color: rgb(0, 0, 0); font-family: Helvetica, sans-serif; font-size: 16px;\">COMPANION</strong><span style=\"color: rgb(0, 0, 0); font-family: Helvetica, sans-serif; font-size: 16px;\">and&nbsp;</span><strong style=\"box-sizing: border-box; color: rgb(0, 0, 0); font-family: Helvetica, sans-serif; font-size: 16px;\">BFF</strong><span style=\"color: rgb(0, 0, 0); font-family: Helvetica, sans-serif; font-size: 16px;\">&nbsp;invented by KAWS.</span><br>', '1.jpg', '1.jpg', '1.jpg', 2, 'In Stock', '2020-11-26 15:44:51', NULL);
 
 -- --------------------------------------------------------
 
@@ -216,24 +172,7 @@ CREATE TABLE `subcategory` (
 --
 
 INSERT INTO `subcategory` (`id`, `categoryid`, `subcategory`, `creationDate`, `updationDate`) VALUES
-(13, 7, 'Kaos', '2020-11-26 15:36:56', NULL),
-(14, 15, 'Альгинатные маски для лица', '2022-06-12 08:44:28', NULL),
-(15, 15, 'Ампульные комплексы для лица', '2022-06-12 08:44:57', NULL),
-(16, 11, 'Гарниры', '2022-06-12 08:46:10', NULL),
-(17, 11, 'Сладости', '2022-06-12 08:49:38', NULL),
-(18, 11, 'Масла, соусы, уксусы', '2022-06-12 08:49:51', NULL),
-(19, 11, 'Напитки', '2022-06-12 08:50:21', NULL),
-(20, 11, 'Суперфуды', '2022-06-12 08:50:30', NULL),
-(21, 16, 'Косметика', '2022-06-12 08:51:38', NULL),
-(22, 16, 'Для дома', '2022-06-12 08:51:48', NULL),
-(23, 16, 'Для детей', '2022-06-12 08:52:01', NULL),
-(24, 16, 'Пробники', '2022-06-12 08:52:53', NULL),
-(25, 20, 'Декоративная косметика', '2022-06-12 08:54:59', NULL),
-(26, 20, 'Подарки и подарочные наборы', '2022-06-12 09:00:45', NULL),
-(27, 20, 'Пробники', '2022-06-12 09:01:02', NULL),
-(28, 21, 'Для тела', '2022-06-12 09:01:27', NULL),
-(29, 21, 'Для тела', '2022-06-12 09:03:17', NULL),
-(30, 21, 'Для тела', '2022-06-12 09:03:27', NULL);
+(13, 7, 'Kaos', '2020-11-26 15:36:56', NULL);
 
 -- --------------------------------------------------------
 
@@ -260,10 +199,7 @@ INSERT INTO `userlog` (`id`, `userEmail`, `userip`, `loginTime`, `logout`, `stat
 (26, 'kedua@gmail.com', 0x3a3a3100000000000000000000000000, '2020-11-26 16:02:02', '26-11-2020 09:33:06 PM', 1),
 (27, 'coba@gmail.com', 0x3a3a3100000000000000000000000000, '2020-11-26 16:03:25', '26-11-2020 09:33:32 PM', 1),
 (28, 'coba@gmail.com', 0x3a3a3100000000000000000000000000, '2020-11-26 16:03:52', '26-11-2020 09:35:57 PM', 1),
-(29, 'almaopt.1@yandex.ru', 0x3132372e302e302e3100000000000000, '2022-06-09 11:00:50', NULL, 1),
-(30, 'almaopt.1@yandex.ru', 0x3132372e302e302e3100000000000000, '2022-06-11 16:24:57', '11-06-2022 07:36:38 PM', 1),
-(31, 'almaopt.1@yandex.ru', 0x3132372e302e302e3100000000000000, '2022-06-12 03:52:06', NULL, 1),
-(32, 'almaopt.1@yandex.ru', 0x3132372e302e302e3100000000000000, '2022-06-12 13:49:11', NULL, 1);
+(29, 'almaopt.1@yandex.ru', 0x3132372e302e302e3100000000000000, '2022-06-09 11:00:50', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -397,19 +333,19 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT для таблицы `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT для таблицы `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT для таблицы `ordertrackhistory`
 --
 ALTER TABLE `ordertrackhistory`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT для таблицы `productreviews`
@@ -421,19 +357,19 @@ ALTER TABLE `productreviews`
 -- AUTO_INCREMENT для таблицы `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT для таблицы `subcategory`
 --
 ALTER TABLE `subcategory`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT для таблицы `userlog`
 --
 ALTER TABLE `userlog`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=33;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
